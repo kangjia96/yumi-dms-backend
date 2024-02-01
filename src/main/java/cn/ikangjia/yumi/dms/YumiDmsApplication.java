07d9549d41e954629e4773dccbe1006b20b3aafc
kangjia <ikangjia.cn@outlook.com>
2024-02-01 11:06:57 +0800

    init:  init and add dependencies

---
+++ src/main/java/cn/ikangjia/yumi/dms/YumiDmsApplication.java
@@ -0,0 +1,13 @@
+package cn.ikangjia.yumi.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumiDmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YumiDmsApplication.class, args);
+    }
+
+}
